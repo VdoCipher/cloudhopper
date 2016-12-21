600f7f5f9687efbde980b3fdf563ee471d2a8c16
Vibhav Sinha <v@vsinha.me>
2016-12-21 17:05:12 +0530

    add build file

--- build/cloudhopper.js
+++ build/cloudhopper.js
@@ -60,6 +60,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	  value: true
 	});
 
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -155,8 +157,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	          query: event.queryStringParameters
 	        };
 	      }
-	      if (req.body !== '') {
-	        console.log(event.body);
+	      if (req.body !== '' && _typeof(req.body) !== 'object') {
 	        req.body = JSON.parse(req.body);
 	      }
 
